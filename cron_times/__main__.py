fa8e6d1215bb0b6703d11bb87f9bccfcfd41f875
tzing <tzingshih@gmail.com>
2023-01-18 14:37:53 +0800

    wip: add dbt provider

--- cron_times/__main__.py
+++ cron_times/__main__.py
@@ -1,5 +1,6 @@
+from pathlib import Path
+
 import click
-import cron_times
 
 
 @click.group()
@@ -27,7 +28,44 @@ def serve(host: str, port: int):
     """Run server in dev mode.
 
     Note: for production, use a production WSGI server instead."""
+    import cron_times
+
     cron_times.app.run(host=host, port=port, debug=True)
+
+
+@cli.group()
+def get_tasks():
+    """Fetch cronjobs."""
+    ...
+
+
+@get_tasks.command()
+@click.option(
+    "--file",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+    show_default=True,
+    default="tasks/dbt.yaml",
+    help="File to output or update.",
+)
+@click.option("--account-id", required=True, type=click.INT, help="Account id")
+@click.option(
+    "--token",
+    required=True,
+    envvar="DBT_TOKEN",
+    help="API token. Could be provided from env var `DBT_TOKEN`.",
+)
+@click.option(
+    "-p",
+    "--project-id",
+    type=click.INT,
+    help="Only select task(s) in this project. "
+    "You can set this option multiple times. All tasks would be included when on set",
+)
+def dbt(account_id: int, token: str):
+    """Get cronjobs from dbt."""
+    import cron_times.providers.dbt
+
+    tasks = cron_times.providers.dbt.get_tasks(account_id=account_id, token=token)
 
 
 if __name__ == "__main__":
